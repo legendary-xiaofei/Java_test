9d263edbea423111659e3ef8cf28b7e7a310286a
1234 <myfuture_ltf@126.com>
2019-06-07 16:45:55 +0800

    Daily coding exercises on LeetCode

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,22 @@
-import java.util.Arrays;
+import E100道高频.Daily_Temperatures;
+import Exercise.Find_Mode_in_Binary_Search_Tree;
+
 
 public class Main {
 
      public static void main(String[] args) {
 
-         int i=255;
 
-         System.out.println((byte)(i&0xFF));
-         System.out.println("其二进制为："+int2BinaryString(-3));
-         System.out.println(Arrays.toString(int2ByteArray(i)));
+     }
 
+
+    public static void tes( String s,int k){
+        if (k<=s.length()){
+            String s1=s.substring(k);
+            System.out.println(s1);
+            tes(s1,1);
+        }
     }
-    private static byte[] int2ByteArray(int i){
-        byte[] result=new byte[4];
-        result[0]=(byte)((i >> 24)& 0xFF);
-        result[1]=(byte)((i >> 16)& 0xFF);
-        result[2]=(byte)((i >> 8)& 0xFF);
-        result[3]=(byte)(i & 0xFF);
-        return result;
-    }
-    private static String int2BinaryString(int i){
-        String result;
 
-        result =Integer.toBinaryString(i);
-
-
-        return result;
-
-    }
 
 }
