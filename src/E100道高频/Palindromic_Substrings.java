67af289fdb5cd4772c3b28a34c9ce712009312cf
1234 <myfuture_ltf@126.com>
2019-06-07 21:09:49 +0800

    Daily coding exercises on LeetCode

---
+++ src/E100道高频/Palindromic_Substrings.java
@@ -0,0 +1,36 @@
+package E100道高频;
+
+/**
+ * 求最长回文子串
+ *
+ * 示例 1:
+ *
+ * 输入: "abc"
+ * 输出: 3
+ * 解释: 三个回文子串: "a", "b", "c".
+ *
+ * 示例 2:
+ *
+ * 输入: "aaa"
+ * 输出: 6
+ * 说明: 6个回文子串: "a", "a", "a", "aa", "aa", "aaa".
+ */
+public class Palindromic_Substrings {
+    private int num=0;
+    public int countSubstrings(String s) {
+        for (int i=0; i < s.length(); i++){
+            count(s, i, i);//回文串长度为奇数
+            count(s, i, i+1);//回文串长度为偶数
+        }
+        return num;
+    }
+
+    private void count(String s, int start, int end){
+        while(start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)){
+            num++;
+            start--;
+            end++;
+        }
+    }
+
+}
