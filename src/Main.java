479b8293fe3a8b9fdd097087464597e3acaeb340
1234 <myfuture_ltf@126.com>
2019-07-07 22:32:09 +0800

    Daily coding exercises on LeetCode

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,6 @@ public class Main {
 
      public static void main(String[] args) {
 
-         int[]test0={2,4,5,1,3};
-         int []test1={9,8,7,6,6,4,3,2,1};
-         int []test2={4,5,6,7,0,1,2};
-         int []test3={1,6,7,9,10};
-
-//         System.out.println(new Search_in_Rotated_Sorted_Array().search(te,3));
-//         int mode=0x1 << 1;
-//
-//         System.out.print(mode);
-
-         new QuickSort().quickSort(test1,0,test1.length-1);
-
-         for (int i:test1
-              ) {
-             System.out.print(i+" ");
-         }
      }
 
 }
--- src/Sort_algorithm/QuickSort.java
+++ src/Sort_algorithm/QuickSort.java
@@ -4,6 +4,15 @@ public class QuickSort {
 
 
     public void quickSort(int []arr,int left,int right){
+
+        /**
+         * 测试用例：
+         *          int[]test0={2,4,5,1,3};
+         *          int []test1={9,8,7,6,6,4,3,2,1};
+         *          int []test2={4,5,6,7,0,1,2};
+         *          int []test3={1,6,7,9,10};
+         *
+         */
         if (left>=right||left>=arr.length-1)return;
 
         int pr_right=right;
