868eda3b7d1ba9a65c35dae8c9d4badf017a9e4f
1234 <myfuture_ltf@126.com>
2019-07-16 11:22:57 +0800

    Daily coding exercises on LeetCode

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,13 @@
 import DP.Minimum_Path_Sum;
 import E100_High_Fequence.Unique_Paths;
 
+import java.util.Arrays;
+
 public class Main {
 
      public static void main(String[] args) {
 
-          int [][]test={{1,3,1},{1,5,1},{4,2,1}};
-          int[][]test2={{1,0}};
 
-
-          int [][]test3={{0,0},{1,1},{0,0}};
-          int [][]test4={{0,1,0},{0,1,0}};
-          int [][]test5={{0,0},{1,0}};
-//          System.out.println("高是"+test2.length);
-//          System.out.println("宽是"+test2[0].length);
-          System.out.print(new Minimum_Path_Sum().minPathSum(test));
      }
 
 
---
+++ src/Sort_algorithm/HeapSort.java
@@ -0,0 +1,13 @@
+package Sort_algorithm;
+
+public class HeapSort {
+
+    public int [] headsort(int []nums){
+
+
+
+
+        return nums;
+    }
+
+}
