9d6a3222587f0b1872b363679938d44953e826c2
1234 <myfuture_ltf@126.com>
2019-07-17 21:22:18 +0800

    Daily coding exercises on LeetCode

--- src/Exercise/Missing_Number.java
+++ src/Exercise/Missing_Number.java
@@ -1,6 +1,15 @@
 package Exercise;
 
 public class Missing_Number {
+    /**
+     * 给定一个包含n个正整数的数组,理应包含从0到n,一共n+1个数,但是少了其中一个
+     * 请找出其中缺少哪个数
+     *
+     * 如给定{3,0,1}那么输出:2
+     *
+     * @param nums 给定的数组
+     * @return 返回数组中缺少的数字
+     */
 
     public int missingNumber(int[] nums) {
 
